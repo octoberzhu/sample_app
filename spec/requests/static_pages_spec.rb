5211ff5493434537bc411a78b4aac9f66f72b75e
apple <apple@appletekiMacBook-Air.local>
2013-10-18 05:05:57 +0800

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,9 +15,13 @@ describe 'Static pages' do
             visit '/static_pages/home'
             expect(page).to have_content('Sample App')
         end
-        it "Should have the right title 'Home'" do
+        it "Should have the base title" do
             visit '/static_pages/home'
-            expect(page).to have_title('Sample App | Home')
+            expect(page).to have_title('Sample App')
+        end
+        it "Should not have a custom page title" do
+            visit '/static_pages/home'
+            expect(page).not_to have_title('| Home')
         end
     end
     describe 'Help page' do
