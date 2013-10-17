c9e18beaef1859455cc70f0b8db46fbe450aab8e
apple <apple@appletekiMacBook-Air.local>
2013-10-18 00:56:27 +0800

    finish c3:static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+#describe "StaticPages" do
+  #describe "GET /static_pages" do
+    #it "works! (now write some real specs)" do
+      ## Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      #get static_pages_index_path
+      #response.status.should be(200)
+    #end
+  #end
+#end
+describe 'Static pages' do
+    describe 'Home page' do
+        it "should have the content 'Sample App'" do
+            visit '/static_pages/home'
+            expect(page).to have_content('Sample App')
+        end
+        it "Should have the right title 'Home'" do
+            visit '/static_pages/home'
+            expect(page).to have_title('Sample App | Home')
+        end
+    end
+    describe 'Help page' do
+        it "Should have the content 'Help'" do
+            visit '/static_pages/help'
+            expect(page).to have_content('Help')
+        end
+        it "Should have the right title 'Help'" do
+            visit '/static_pages/help'
+            expect(page).to have_title('Sample App | Help')
+        end
+    end
+    describe 'About page' do
+        it "Should have the content 'About Us'" do
+            visit '/static_pages/about'
+            expect(page).to have_content('About Us')
+        end
+        it "Should have the right title 'About Us'" do
+            visit '/static_pages/about'
+            expect(page).to have_title('Sample App | About Us')
+        end
+    end
+
+end
